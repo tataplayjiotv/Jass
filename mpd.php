d5ae8f87588d7de26acc54cb6d9e28484296be78
tataplayjiotv <saini000987098@gmail.com>
2024-08-19 08:27:38 +0530

    Update mpd.php

--- mpd.php
+++ mpd.php
@@ -2,16 +2,9 @@
 
 error_reporting(0);
 date_default_timezone_set('Asia/Kolkata');
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
- 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
- 
+
 if (empty($id)) {
     http_response_code(400);
     echo json_encode(["error" => "Missing or invalid 'id' parameter"]);
@@ -24,44 +17,75 @@ function fetchContent($url) {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
     if (curl_errno($ch)) {
+        curl_close($ch);
         return null;
     }
     curl_close($ch);
     return $response;
 }
 
-$id = basename($id);
+function base64url_encode($data) {
+    return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+}
 
+$id = basename($id);
 $cache_dir = "_cache_/";
 $cacheTime = 60;
-$cacheFile = $cache_dir . "TP-$id.mpd";
+$cacheFile = $cache_dir . "TP-$id.json";
+
+if (!file_exists($cache_dir)) {
+    mkdir($cache_dir, 0777, true);
+}
 
 if (file_exists($cacheFile) && (time() - filemtime($cacheFile)) < $cacheTime) {
-    header('Content-Type: application/dash+xml');
+    header('Content-Type: application/json');
     readfile($cacheFile);
     exit;
 }
 
-$data = json_decode(fetchContent("https://fox.toxic-gang.xyz/tata/key/$id"), true);
-$initialUrl = $data[0]['data']['https://bpweb4.akamaized.net/bpk-tv/irdeto_com_Channel_221/output/toxicify.mpd'];
-$psshSet = $data[0]['data']['psshSet'];
-$kid = $data[0]['data']['kid'];
-$bssh = preg_replace('/bpweb(\d+)\.akamaized\.net/', 'bpprod$1catchup.akamaized.net', $initialUrl);
-$bssh = preg_replace('/bpweb(\d+)\.akamaized-staging\.net/', 'bpprod$1catchup.akamaized.net', $bssh);
-$pattern = '/<ContentProtection\s+schemeIdUri="(urn:[^"]+)"\s+value="Widevine"\/>/';
+$content = fetchContent("https://fox.toxic-gang.xyz/tata/key/$id");
 
+if ($content === null) {
+    http_response_code(500);
+    echo json_encode(["error" => "Failed to fetch data from the remote server"]);
+    exit;
+}
 
-$manifestContent = fetchContent($bssh);
-$manifestContent = str_replace('<BaseURL>dash/</BaseURL>', '<BaseURL>' . str_replace("toxicify.mpd","dash/",$bssh) . '</BaseURL>', $manifestContent);
-$manifestContent = preg_replace('/\b(init.*?\.dash|media.*?\.m4s)(\?idt=[^"&]*)?("|\b)(\?decryption_key=[^"&]*)?("|\b)(&idt=[^&"]*(&|$))?/', "$1$3$5$6$7", $manifestContent);
-$manifestContent = preg_replace_callback($pattern, function ($matches) use ($psshSet) {
-    return '<ContentProtection schemeIdUri="' . $matches[1] . '"> <cenc:pssh>' . $psshSet . '</cenc:pssh></ContentProtection>';
-}, $manifestContent);
-$manifestContent = preg_replace('/xmlns="urn:mpeg:dash:schema:mpd:2011"/', '$0 xmlns:cenc="urn:mpeg:cenc:2013"', $manifestContent);
-$new_content = "<ContentProtection schemeIdUri=\"urn:mpeg:dash:mp4protection:2011\" value=\"cenc\" cenc:default_KID=\"$kid\"/>"; 
-$manifestContent = str_replace('<ContentProtection value="cenc" schemeIdUri="urn:mpeg:dash:mp4protection:2011"/>', $new_content, $manifestContent);
+$data = json_decode($content, true);
 
+if ($data === null) {
+    http_response_code(500);
+    echo json_encode(["error" => "Invalid JSON response from the remote server"]);
+    exit;
+}
 
-header('Content-Type: application/dash+xml');
-file_put_contents($cacheFile, $manifestContent);
-echo $manifestContent;
+$kid = $data['data']['licence1'];
+$key = $data['data']['licence2'];
+
+$binary_kid = hex2bin($kid);
+$binary_k = hex2bin($key);
+
+if ($binary_kid === false || $binary_k === false) {
+    http_response_code(500);
+    echo json_encode(["error" => "Invalid hexadecimal input"]);
+    exit;
+}
+
+$encoded_kid = base64url_encode($binary_kid);
+$encoded_k = base64url_encode($binary_k);
+
+$response = [
+    "keys" => [
+        [
+            "kty" => "oct",
+            "k" => $encoded_k,
+            "kid" => $encoded_kid
+        ]
+    ],
+    "type" => "temporary"
+];
+
+header('Content-Type: application/json');
+file_put_contents($cacheFile, json_encode($response));
+echo json_encode($response);
+?>
