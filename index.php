a31dcde90efbfbeafd5b675d4c470523334b4d45
tataplayjiotv <saini000987098@gmail.com>
2025-01-20 16:45:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+cookie:
+country=IN; display-language=en; variant=v2; xaccesstoken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwbGF0Zm9ybV9jb2RlIjoiV2ViQCQhdDM4NzEyIiwiaXNzdWVkQXQiOiIyMDI1LTAxLTIwVDA3OjQyOjM3LjU4N1oiLCJwcm9kdWN0X2NvZGUiOiJ6ZWU1QDk3NSIsInR0bCI6ODY0MDAwMDAsImlhdCI6MTczNzM1ODk1N30.z665KcIY3m7ZyElebCKoOlyIZKpQKt5lLqpiflcneSg; content-language=en,hi,pa; state=CH; _cc_id=7504c4f943c3d7f019a94f1471037e45; panoramaId_expiry=1737449566025; panoramaId=d2fa106f9c0f358059b3706f71c0a9fb927af114de5b4fdde2ed5fbdf65e82a4; panoramaIdType=panoDevice; _fbp=fb.1.1737363176572.116432717529513042; WZRK_G=e87a57de2fe14bfbb88d8879b05446d0; afUserId=08746702-ff2e-4e07-9e6d-cdce7bbdb5ac-p; __gads=ID=c90b2f64aadd18bc:T=1737363176:RT=1737363176:S=ALNI_MYFrLW_NsoVVoJKAqgiepBV8vWnMg; __eoi=ID=6d9d07a5c1c48198:T=1737363176:RT=1737363176:S=AA-AfjbedKggx-ZrysaWERUCwQhj; _gcl_aw=GCL.1737363179.Cj0KCQiAhbi8BhDIARIsAJLOlud6aDr9ODlcuTY6IznY1XhW8sa_e0pCM7DYpg0qHQXsR73jlwZXz_IaAkamEALw_wcB; _gcl_dc=GCL.1737363179.Cj0KCQiAhbi8BhDIARIsAJLOlud6aDr9ODlcuTY6IznY1XhW8sa_e0pCM7DYpg0qHQXsR73jlwZXz_IaAkamEALw_wcB; _gcl_gs=2.1.k1$i1737363142$u183436589; _gcl_au=1.1.137678117.1737363179; affiliate_name=self; isUserActive=yes; cjConsent=MHxOfDB8Tnww; cjUser=a0fe94fc-8b57-41dc-b5cb-b97575c42041; zutype=guest; _scid=L6nReYDVnHpRXGn_-xr9EAS-nfUf_Toa; _gid=GA1.2.487200987.1737363194; hcsmlc=google-organic; _ScCbts=%5B%5D; _tt_enable_cookie=1; _ttp=0XmqiNS-x5xVjiHrNw478NzENAY.tt.1; _sctr=1%7C1737311400000; _sp_ses.60de=*; G_ENABLED_IDPS=google; AF_SYNC=1737364988792; cto_bundle=6Nq6QV9tYkRiR2MlMkJjaWpjNnExWlp0b053OElGdmlyMGIzQzRnanZVd0MyZDFyWTd1WkYyV1ZVZkJsUjZ6QTJjbktzZWVCdDVBZVdZbWRNM3RDcHUlMkJOVERZZUtTdEM5VmFpcEpBNXkxN0VVWjJEcjNaeGhXaW9sYXhTOERFa3pHdXQlMkZlWW9Eam41OUR6M0ptTzlKbTBlWEVVSFElM0QlM0Q; hcsid=6e67b1ddff2f49ab; user-type=register; refreshToken=c96634a2195a11d9da64d637a84db0f07c6cdf3f82760a64844056cdf91cde26; _ga_KJ9WPD489P=GS1.1.1737365258.1.1.1737365280.0.0.0; _sp_id.60de=b52a6481-ade2-4a30-a30d-58f7bc299812.1737363459.1.1737365298..8ef65bf9-cf7e-43d3-bfc0-4b34d3e537ec..62458223-c229-4440-a4a8-08e1240d6019.1737363458699.155; 
+
+
+earer eyJraWQiOiJkZjViZjBjOC02YTAxLTQ0MWEtOGY2MS0yMDllMjE2MGU4MTUiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzQjhGQUE3Ny03M0IxLTQ5M0YtODQ1Qi1CQjc1QjlFRkFFQzIiLCJkZXZpY2VfaWQiOiJiNDliZDg5Mi1kNzQ4LTQzNmYtYWNjZS0zOGQ5OGRkNmViMzciLCJhbXIiOlsiZGVsZWdhdGlvbiJdLCJ0YXJnZXRlZF9pZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6Ly91c2VyYXBpLnplZTUuY29tIiwidmVyc2lvbiI6MTAsImNsaWVudF9pZCI6InJlZnJlc2hfdG9rZW4iLCJhdWQiOlsidXNlcmFwaSIsInN1YnNjcmlwdGlvbmFwaSIsInByb2ZpbGVhcGkiLCJnYW1lLXBsYXkiXSwidXNlcl90eXBlIjoiUmVnaXN0ZXJlZCIsIm5iZiI6MTczNzM2NTI3MiwidXNlcl9pZCI6IjNiOGZhYTc3LTczYjEtNDkzZi04NDViLWJiNzViOWVmYWVjMiIsInNjb3BlIjpbInVzZXJhcGkiLCJzdWJzY3JpcHRpb25hcGkiLCJwcm9maWxlYXBpIl0sInNlc3Npb25fdHlwZSI6IkdFTkVSQUwiLCJleHAiOjE3Mzc3MTA4NzIsImlhdCI6MTczNzM2NTI3MiwianRpIjoiNmZiOTk4ZTItYjU5MC00OWRmLWFiNjYtMzZlMDcwYWU5MzA3In0.B93VkPT1G2AKcbwJp07Yxgcvk4xG9L4pdAmQFCb4vHD5GauOIonRq394zyn8ADz-UwA-62Z0r5zYr68qTrFWKdB4G0GTZ8zMm-6Cjokl-UYxEvHe9jiBBr7vqVKQLYeh11iMMdB6IqXx3mc-jovUzCqnj5AUCpLbdoYyh8qaDe0OSHwx0mnO8HIgnsHMKDMXFGkD44oafyJ1EPLVxB2xamFykLwBqg-sneavZsmRn8rTg6DrYZ6ZZON1T_AfpL_XzCKog-lzTB7iMGsl-ORmnSyFMP2UAae8WMJHQZ0dvwE27fWUgdzEF07j4FHUxqKGbnWXu2GIbXlo4EpYJyaUnA
+cache-control:
+no-cache
+content-length:
+41
+content-type:
+application/json
+device_id:
+b49bd892-d748-436f-acce-38d98dd6eb37
+esk:
+YjQ5YmQ4OTItZDc0OC00MzZmLWFjY2UtMzhkOThkZDZlYjM3X19nQlFhWkxpTmRHTjlVc0NLWmFsb2doejl0OVN0V0xTRF9fMTczNzM2NTUyNjcyOA==
+origin:
+https://www.zee5.com
+pragma:
+no-cache
+priority:
+u=1, i
+referer:
+https://www.zee5.com/
+sec-ch-ua:
+"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"
+sec-ch-ua-mobile:
+?0
+sec-ch-ua-platform:
+"Windows"
+sec-fetch-dest:
+empty
+sec-fetch-mode:
+cors
+sec-fetch-site:
+same-site
+user-agent:
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36
+x-z5-appplatform:
+Web Desktop
+x-z5-appversion:
+4.16.2
+
+Request URL:
+https://spapi.zee5.com/singlePlayback/getDetails/secure?channel_id=0-9-209&device_id=b49bd892-d748-436f-acce-38d98dd6eb37&platform_name=desktop_web&translation=en&user_language=en,hi,pa&country=IN&state=CH&app_version=4.16.2&user_type=register&check_parental_control=false&gender=Male&age_group=25-32&utm_source=GoogleSearch&uid=3b8faa77-73b1-493f-845b-bb75b9efaec2&ppid=b49bd892-d748-436f-acce-38d98dd6eb37&version=12
+Request Method:
+POST
+Status Code:
+401 Unauthorized
+Remote Address:
+96.17.168.64:443
+Referrer Policy:
+strict-origin-when-cross-origin
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+_scid_r=KinReYDVnHpRXGn_-xr9EAS-nfUf_ToatgIKUQ; MgidSensorNVis=12; MgidSensorHref=https://www.zee5.com/live-tv/lang/hindi-channels; _uetsid=00d08fe0d70c11ef84f43d31d090c713; _uetvid=00df3230d70c11efafadb54a0b81e583; _ga=GA1.1.1591036611.1737363187; FCNEC=%5B%5B%22AKsRol8AN4L8MibI8JnrIBDcG4TqqjtcnpeI1_qdnT-VbZOgdzd-xfhHJII-ieFsf8lxaWAYTHkDIadyvGep1CirprfHmSnm6XCTvouVGmeTsY5iG9_HPbVr1hNNt0sB_AK0bYNsXotxTdg8luBZhCfHZM9cN7e-Xw%3D%3D%22%5D%5D; WZRK_S_RKW-4R7-785Z=%7B%22p%22%3A4%2C%22s%22%3A1737363179%2C%22t%22%3A1737365499%7D; _ga_LB5CP1CVNF=GS1.1.1737363191.1.1.1737365499.60.0.0; _ga_T0DHWCDQTE=GS1.1.1737363186.1.1.1737365499.60.0.0
